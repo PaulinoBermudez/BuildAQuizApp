02a277f3a19a7da8a2bdaca2f4dc5810d44ebe1b
chunche95 <paeste95.pb@gmail.com>
2020-02-28 02:57:55 +0100

    Finish page of the quiz project

--- end.js
+++ end.js
@@ -1,16 +1,17 @@
+/* Constants */
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
-
+/* Your note */
 finalScore.innerText = mostRecentScore;
-
+/* Register username */
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
-
+/* Scores */
 saveHighScore = e => {
   console.log("clicked the save button!");
   e.preventDefault();
